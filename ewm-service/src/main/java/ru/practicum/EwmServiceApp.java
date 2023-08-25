afc8e70f058331a236ac6436664838ccd0286449
Dmitry <ku2uym12@yandex.ru>
2023-08-25 12:35:21 +0300

    Этап 1

--- ewm-service/src/main/java/ru/practicum/EwmServiceApp.java
+++ ewm-service/src/main/java/ru/practicum/EwmServiceApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EtmServiceApp {
+public class EwmServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(EtmServiceApp.class, args);
+        SpringApplication.run(EwmServiceApp.class, args);
     }
 }
