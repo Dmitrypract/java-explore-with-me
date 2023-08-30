87e4fcb16b0ef22c4dfbd34db91d537a7d2423f8
Dmitry <ku2uym12@yandex.ru>
2023-08-30 15:42:20 +0300

    Этап 2

--- ewm-service/src/main/java/ru/practicum/main/MainServiceApplication.java
+++ ewm-service/src/main/java/ru/practicum/main/MainServiceApplication.java
@@ -3,9 +3,9 @@ package ru.practicum.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ru.practicum"})
 public class MainServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ru.practicum.main.MainServiceApplication.class, args);
+        SpringApplication.run(MainServiceApplication.class, args);
     }
 }
