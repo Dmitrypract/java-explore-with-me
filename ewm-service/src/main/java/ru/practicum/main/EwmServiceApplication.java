0e8ceaadbab0f281b914be55f6c429ee426c25df
Dmitry <ku2uym12@yandex.ru>
2023-09-02 15:12:51 +0300

    Этап 1

--- ewm-service/src/main/java/ru/practicum/main/EwmServiceApplication.java
+++ ewm-service/src/main/java/ru/practicum/main/EwmServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"ru.practicum"})
-public class MainServiceApplication {
+public class EwmServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainServiceApplication.class, args);
+        SpringApplication.run(EwmServiceApplication.class, args);
     }
 }
