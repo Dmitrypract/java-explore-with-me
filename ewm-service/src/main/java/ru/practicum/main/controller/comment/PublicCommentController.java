a67029e5616ee7bd485ab7181181ac62457c7f41
Dmitry <ku2uym12@yandex.ru>
2023-09-05 16:24:09 +0300

    Этап 3

---
+++ ewm-service/src/main/java/ru/practicum/main/controller/comment/PublicCommentController.java
@@ -0,0 +1,26 @@
+package ru.practicum.main.controller.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.main.dto.comment.FullCommentDto;
+import ru.practicum.main.service.CommentService;
+
+import javax.validation.constraints.Positive;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
+@Validated
+public class PublicCommentController {
+    private final CommentService commentService;
+
+    @GetMapping
+    public Collection<FullCommentDto> getCommentsByEventId(@Positive @RequestParam Long eventId) {
+        return commentService.getCommentsByEventId(eventId);
+    }
+}
